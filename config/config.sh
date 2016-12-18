33785eba43a379df9b1d669e335c8eb062301717
olegrybtsov <oleg.rybtsov@gmail.com>
2016-12-19 00:41:25 +0200

    Update config.sh

--- config/config.sh
+++ config/config.sh
@@ -1,1 +1,7 @@
 #/bin/bash
+mkdir my_app
+cd my_app
+git clone https://github.com/olegrybtsov/app.git
+mv app/config/server.py ./
+mv app/config/wsgi.py ./
+mv app/config/nginx.conf /etc/nginx/nginx.conf -f
