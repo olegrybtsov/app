b9b7a155f610a2e99075bfd1b5e700572b70f134
olegrybtsov <oleg.rybtsov@gmail.com>
2016-12-20 16:36:31 +0200

    m

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
+import cherrypy
+import os
+
 class HelloWorld(object):
     @cherrypy.expose
     def index(self):
-        return "hello python world"
+        return "hello world"
 
 if __name__ == '__main__':
     cherrypy.config.update({'server.socket_host': '0.0.0.0', 'server.socket_port': 80})
