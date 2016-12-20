c9c8587b77117fb582c52ff84a6791e2ce5d3d05
olegrybtsov <oleg.rybtsov@gmail.com>
2016-12-20 15:10:44 +0200

    m

---
+++ server.py
@@ -0,0 +1,8 @@
+class HelloWorld(object):
+    @cherrypy.expose
+    def index(self):
+        return "hello python world"
+
+if __name__ == '__main__':
+    cherrypy.config.update({'server.socket_host': '0.0.0.0', 'server.socket_port': 80})
+    cherrypy.quickstart(HelloWorld())
